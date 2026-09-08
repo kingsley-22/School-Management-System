ed075b42f17608fdf6411695a131dfd568657126
Jungleman042 <egwuatuikechukwu8@gmail.com>
2026-09-08 13:15:09 +0100

    instructor ui

---
+++ frontend/js/instructor-dashboard.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const sidebar = document.querySelector(".sidebar");
+
+menuToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+
+    document.body.classList.toggle("menu-open");
+});
